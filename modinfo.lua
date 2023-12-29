6f6920836e3ef8f677281d52cb5a0c8f688369ed
jennifershan <79737766+jennifershan@users.noreply.github.com>
2023-12-29 03:18:02 -0500

    Update modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,25 @@
+-- Description
+name = "play-pair-of-birds"
+description = ""
+author = ""
+version = "1.0"
+forumthread = ""
+
+--icon_atlas = "modicon.xml"
+--icon = "modicon.tex"
+
+-- Compatibility
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+hamlet_compatible = false
+
+api_version = 10
+
+client_only_mod = true
+
+--Config
+server_filter_tags = {name, author}
+
+configuration_options = { }
